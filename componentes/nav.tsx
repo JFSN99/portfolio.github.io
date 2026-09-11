639a19fce8c740be35d14f43f98c8b15eac51d58
Juan <juanfsn1999@gmail.com>
2026-09-11 06:15:31 -0300

    'Slide_finalizado'

--- componentes/nav.tsx
+++ componentes/nav.tsx
@@ -20,7 +20,7 @@ export const Menu = () => {
                 <div className="w-full h-0.5 bg-indigo-600"></div>
                 <div className="w-full h-0.5 bg-indigo-600"></div>
             </div>
-            <nav className={`${hideMenu?'hidden':'flex'} flex-col font-Oswald bg-black absolute mt-11.5 w-30 right-0`}>
+            <nav className={`${hideMenu?'hidden':'flex'} flex-col font-Oswald bg-black absolute mt-15 w-30 right-0`}>
                 <div className="p-2 text-center transition-all hover:bg-indigo-600 hover:text-white text-indigo-600 cursor-pointer">Quem sou ?</div>
                 <div className="p-2 text-center text-indigo-600 transition-all hover:bg-indigo-600 hover:text-white cursor-pointer" >Formação</div>
                 <div className="p-2 text-center text-indigo-600 transition-all hover:bg-indigo-600 hover:text-white cursor-pointer">Cursos</div>
--- componentes/slider.tsx
+++ componentes/slider.tsx
@@ -11,7 +11,7 @@ export const Slider = () => {
    useEffect(() => {
         const interval = setInterval(() => {
             SlideCtx?.NextS()
-        }, 5000)
+        }, 200000)
         
         return () => clearInterval(interval)
     }, [])
@@ -23,17 +23,32 @@ export const Slider = () => {
                 
                 <div className={` h-full w-[300vw] flex`} style={{marginLeft:`-${SlideCtx?.slider[0].pos ?? 0}vw`}}>
 
-                    <div className="slide1 h-full w-screen flex flex-col items-center">
-                       
-                        <h1 className="animate-pulse line-height font-Oswald text-4xl my-5 text-center uppercase text-white text-shadow-sm text-shadow-gray-600">Programando com excelência</h1>
+                    <div className="slide1 h-full w-screen flex flex-col md:flex-row">
 
-                        <button className="bg-indigo-600 p-2 rounded-sm font-caacupe cursor-pointer hover:bg-indigo-800">Conheça os meus projetos</button>
-                        <img src="/imgS1.png" className="w-80 h-100 cover" alt="" />
+                        <div className="w-full flex flex-col items-center justify-center md:items-start md:justify-center md:pl-10">
+                            <h1 className="font-Oswald text-4xl text-shadow-lg text-shadow-purple-600 my-5 text-center uppercase text-white md:my-0 md:mb-5  md:text-left lg:text-6xl">Programando com excelência</h1>
+                            <button className="bg-indigo-600 animate-pulse text-shadow-md w-auto text-shadow-blue-600 p-2 rounded-sm font-caacupe cursor-pointer lg:text-xl lg:mt-5 hover:bg-indigo-800">Conheça os meus projetos</button>
+                        </div>
+                      
+                        <img src="/imgS1.png" className="w-80 h-100 mx-auto cover md:w-90 md:h-full" alt="" />
                         
                     </div>
                 
-                    <div className={`slide2 h-full w-screen`}></div>
-                    <div className={`slide3 h-full w-screen`}></div>
+                    <div className={`slide2 h-full w-screen flex flex-col items-center md:flex-row md:justify-between lg:justify-around`}>
+                        <div className="flex flex-col items-center justify-center-center md:pl-10">
+                            <h1 className="text-4xl my-5 font-Oswald text-center text-shadow-lg text-shadow-purple-600 md:text-6xl">Quem sou ?</h1>
+                            <button className="bg-indigo-600 w-20 animate-pulse p-2 rounded-sm font-caacupe cursor-pointer hover:bg-indigo-800 lg:text-xl lg:w-25">Saiba mais</button>
+                        </div>
+                        <img src="/imgS2.png" className="w-70" alt="" />
+                    </div>
+                    <div className={`slide3 h-full w-screen flex flex-col items-center md:flex-row md:justify-around`}>
+                        <div className="flex flex-col items-center">
+                            <h1 className="text-4xl my-5 font-Oswald text-center text-shadow-lg text-shadow-purple-600 md:text-6xl">Se interessou ?</h1>
+                            <button className="bg-indigo-600 animate-pulse  p-2 rounded-sm font-caacupe cursor-pointer hover:bg-indigo-800 lg:text-xl">Entre em contato</button>
+                        </div>
+                        <img src="/imgS3.png" className="w-70" alt="" />
+                        
+                    </div>
                     
                 </div>
                 
