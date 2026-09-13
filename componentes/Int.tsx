c1701b4255c8ea38e4da3d57dad35c3098a70776
Juan <juanfsn1999@gmail.com>
2026-09-13 09:17:49 -0300

    'Ajustes_Int+Insercao_eventos'

--- componentes/Int.tsx
+++ componentes/Int.tsx
@@ -16,15 +16,41 @@ export const Int = () => {
                     <div className="bg-black py-5 px-3">
                     
                         <div className="bg-linear-to-r from-indigo-600 to-purple-600  p-3 rounded-md shadow-lg shadow-blue-500/50 font-Outfit">
-                            <h1 className="text-4xl animate-pulse">Sobre Mim</h1>
+                            <div className="flex justify-between pr-2 bg-">
+                                <h1 className="pl-2 text-4xl animate-pulse">Sobre Mim</h1>
+                                <button className="bg-white px-3 font-bold cursor-pointer text-indigo-600 rounded-sm hover:opacity-60" onClick={IntCtx?.handleInt}>X</button>
+                            </div>
                             <div className="flex justify-between items-center">
-                                <ul className="mt-5">
-                                    <li>Nome: Juan</li>
-                                    <li>Formação 1: Engenharia elétrica - UFRPE (concluída)</li>
-                                    <li>Formação 2: ADS - Estácio (Em andamento)</li>
-                                    <li>Experiência 1: Estagiário Stellantis - 1 ano (06/2023 à 06/2024)</li>
-                                    <li>Experiência 2: Trainee Stellantis - 1 ano (09/2024 à 04/2026) {IntCtx?.sessoes[0].Int}</li>
-                                </ul>
+                                <table className="mt-10">
+                                    <tbody className="**:h-10 *:hover:bg-sky-600">
+                                        <tr className="text-center">
+                                            <td className="px-3 text-left">Nome</td>
+                                            <td className="px-3">Juan</td>
+                                            <td className="px-3 text-right"></td>
+                                        </tr>
+                                        <tr className="text-center">
+                                            <td className="px-3 text-left">Formação 1</td>
+                                            <td className="px-3">Engenharia Elétrica - UFRPE</td>
+                                            <td className="px-3 text-right">Concluído</td>
+                                        </tr>
+                                        <tr className="text-center">
+                                            <td className="px-3 text-left">Formação 2</td>
+                                            <td className="px-3">ADS - Estácio</td>
+                                            <td className="px-3 text-right">Em andamento</td>
+                                        </tr>
+                                        <tr className="text-center">
+                                            <td className="px-3 text-left">Experiência 1</td>
+                                            <td className="px-3">Estagiário Stellantis</td>
+                                            <td className="px-3 text-right"> 1 ano</td>
+                                        </tr>
+                                        <tr className="text-center">
+                                            <td className="px-3 text-left">Experiência 2</td>
+                                            <td className="px-3">Trainee Stellantis</td>
+                                            <td className="px-3 text-right">1 ano e 8 meses</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                                
                                 <img src="/microchip.png" className="w-30 h-30" alt="" />
                             </div>
                         
--- componentes/slider.tsx
+++ componentes/slider.tsx
@@ -2,16 +2,16 @@
 import { useState, useContext, useEffect} from "react"
 import { SlideContext } from "@/contexts/slideCtx"
 import '../app/globals.css'
-
+import { SessaoConst } from "@/contexts/SessoesCtx"
 
 export const Slider = () => {
 
     const SlideCtx = useContext(SlideContext)
-
+    const sessCtx = useContext(SessaoConst)
    useEffect(() => {
         const interval = setInterval(() => {
             SlideCtx?.NextS()
-        }, 200000)
+        }, 5000)
         
         return () => clearInterval(interval)
     }, [])
@@ -27,7 +27,7 @@ export const Slider = () => {
 
                         <div className="w-full flex flex-col items-center justify-center md:items-start md:justify-center md:pl-10">
                             <h1 className="font-Oswald text-4xl text-shadow-lg text-shadow-purple-600 my-5 text-center uppercase text-white md:my-0 md:mb-5  md:text-left lg:text-6xl">Programando com excelência</h1>
-                            <button className="bg-indigo-600 animate-pulse text-shadow-md w-auto text-shadow-blue-600 p-2 rounded-sm font-caacupe cursor-pointer lg:text-xl lg:mt-5 hover:bg-indigo-800">Conheça os meus projetos</button>
+                            <button className="bg-indigo-600 animate-pulse text-shadow-md w-auto text-shadow-blue-600 p-2 rounded-sm font-caacupe cursor-pointer lg:text-xl lg:mt-5 hover:bg-indigo-800" >Conheça os meus projetos</button>
                         </div>
                       
                         <img src="/imgS1.png" className="w-80 h-100 mx-auto cover md:w-90 md:h-full" alt="" />
@@ -37,7 +37,7 @@ export const Slider = () => {
                     <div className={`slide2 h-full w-screen flex flex-col items-center md:flex-row md:justify-between lg:justify-around`}>
                         <div className="flex flex-col items-center justify-center-center md:pl-10">
                             <h1 className="text-4xl my-5 font-Oswald text-center text-shadow-lg text-shadow-purple-600 md:text-6xl">Quem sou ?</h1>
-                            <button className="bg-indigo-600 w-20 animate-pulse p-2 rounded-sm font-caacupe cursor-pointer hover:bg-indigo-800 lg:text-xl lg:w-25">Saiba mais</button>
+                            <button className="bg-indigo-600 w-20 animate-pulse p-2 rounded-sm font-caacupe cursor-pointer hover:bg-indigo-800 lg:text-xl lg:w-25" onClick={sessCtx?.handleInt}>Saiba mais</button>
                         </div>
                         <img src="/imgS2.png" className="w-70" alt="" />
                     </div>
