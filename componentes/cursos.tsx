5882071f35ab2833459c3d839d899871a089b532
Juan <juanfsn1999@gmail.com>
2026-09-17 16:25:01 -0300

    Aprimoramento Aba Cursos

--- componentes/cursos.tsx
+++ componentes/cursos.tsx
@@ -9,20 +9,42 @@ export const Cursos = () => {
     const CrsCtx = useContext(CursosCtx);
     return(
 
-        <div className="mt-3 p-2">
-            {SessCtx?.sessoes[0].cursos && 
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 rounded-sm shadow-md shadow-indigo-500 bg-purple-600 gap-1 md:gap-10 p-2">
-                    {CrsCtx?.map((item) => {
-                        return(
-                            <div className="w-76 h-55 mx-auto p-2 border border-white" key={item.id}>
-                                <div className="flex flex-col p-2 items-center justify-center bg-black mx-auto absolute w-72 h-51 opacity-0 hover:opacity-70">
-                                    <strong className="text-xl text-center">Curso: {item.nome}</strong>
-                                    <strong className="text-xl text-center">Duração: {item.duracao} Horas</strong>
-                                </div>
-                                <img src={`${item.src}`} className="w-full cover mx-auto" alt=""></img>
-                            </div>)
-                    })}
-                </div>
+        <div className="mt-3">
+            {SessCtx?.sessoes[0].cursos &&
+                <>
+                    <div className="bg-black opacity-80 hidden justify-center items-center h-screen w-full absolute">
+                        <div className="bg-red-600 w-10 h-5">
+
+                        </div>
+                    </div>
+                    <div className="flex justify-between pr-2">
+                        <div className="flex flex-col flex-start mb-5">
+                            <h1 className="text-2xl rounded-full">Cursos</h1>
+                            <div className="w-18 h-1 bg-linear-to-r from-cyan-500 to-green-500 rounded-full"></div>
+                        </div>
+                        <button className="bg-white w-7 h-7 font-bold cursor-pointer text-indigo-600 rounded-sm hover:opacity-60" onClick={() => SessCtx.handleCursos()}>X</button>
+                    </div>
+                    <div className="p-2 h-100 overflow-y-scroll">
+                        <div className="h-auto">
+                            
+                            
+                            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-1 md:gap-10 p-2">
+                                {CrsCtx?.map((item) => {
+                                    return(
+                                        <div className="w-68 h-55 md:w-85 md:h-59 lg:w-76 lg:h-55 xl:w-80 xl:h-59 2xl:w-118 2xl:h-97 mx-auto p-2 border border-white flex items-center justify-center" key={item.id}>
+                                            <div className="flex flex-col p-2 items-center justify-center bg-black mx-auto absolute w-66 h-47 opacity-0 hover:opacity-70">
+                                                <strong className="text-xl text-center">Curso: {item.nome}</strong>
+                                                <strong className="text-xl text-center">Duração: {item.duracao} Horas</strong>
+                                            </div>
+                                            <img src={`${item.src}`} className="w-full h-full cover mx-auto" alt=""></img>
+                                        </div>)
+                                })}
+                            </div>
+                        </div>
+                        
+                    </div>
+                </>
+                
             }
         </div>
 
