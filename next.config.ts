d91072eac01cc9bef073a11bac8da460fa2966a8
Juan <juanfsn1999@gmail.com>
2026-09-25 11:01:02 -0300

    'Fix-correcao'

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode:false,
-  output: 'export', // Diz ao Next para gerar HTML/CSS estáticos
-  basePath: '/nome-do-seu-repositorio', // OBRIGATÓRIO: barra '/' seguida pelo nome exato do repositório
-  images: {
-    unoptimized: true, // O GitHub Pages não suporta a otimização de imagens padrão do Next.js
-  },
+  reactStrictMode:false
 };
 
 export default nextConfig;
