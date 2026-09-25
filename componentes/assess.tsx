ee1f1e3a35e6a0dba2b45fdf99665ae648319b83
Juan <juanfsn1999@gmail.com>
2026-09-24 23:50:51 -0300

    'sess_habilidade_finalizada'

--- componentes/assess.tsx
+++ componentes/assess.tsx
@@ -8,46 +8,56 @@ const HandleNota = (nota:number) => {
     switch(nota){
         case 1:
             return(
-                <div>
-                    <img className="w-10 h-10"  src="https://uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" />
+                <div className="flex gap-1 justify-center items-center">
+                    <img className="w-8 h-8"  src="https://uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" />
+                    <img className="w-8 h-8" src="https://uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-empty-icon.png" alt="" />
+                    <img className="w-8 h-8" src="https://uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-empty-icon.png" alt="" />
+                    <img className="w-8 h-8" src="https://uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-empty-icon.png" alt="" />
+                    <img className="w-8 h-8" src="https://uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-empty-icon.png" alt="" />
                 </div>
             )
         break;
         case 2:
             return(
-                <div className="flex">
-                    <img className="w-10 h-10"  src="https://uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" />
-                    <img className="w-10 h-10"  src="https://uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" />
+                <div className="flex gap-1">
+                    <img className="w-8 h-8"  src="https://uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" />
+                    <img className="w-8 h-8"  src="https://uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" />
+                    <img className="w-8 h-8" src="https://uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-empty-icon.png" alt="" />
+                    <img className="w-8 h-8" src="https://uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-empty-icon.png" alt="" />
+                    <img className="w-8 h-8" src="https://uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-empty-icon.png" alt="" />
                 </div>
             )
         break;
         case 3:
             return(
-                <div className="flex">
-                    <img className="w-10 h-10"  src="https://uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" />
-                    <img className="w-10 h-10"  src="https://uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" />
-                    <img className="w-10 h-10"  src="https://uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" />
+                <div className="flex gap-1">
+                    <img className="w-8 h-8"  src="https://uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" />
+                    <img className="w-8 h-8"  src="https://uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" />
+                    <img className="w-8 h-8"  src="https://uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" />
+                    <img className="w-8 h-8" src="https://uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-empty-icon.png" alt="" />
+                    <img className="w-8 h-8" src="https://uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-empty-icon.png" alt="" />
                 </div>
             )
         break;
         case 4:
             return(
-                <div className="flex">
-                    <img className="w-10 h-10"  src="https://uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" />
-                    <img className="w-10 h-10"  src="https://uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" />
-                    <img className="w-10 h-10"  src="https://uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" />
-                    <img className="w-10 h-10"  src="https://uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" />
+                <div className="flex gap-1">
+                    <img className="w-8 h-8"  src="https://uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" />
+                    <img className="w-8 h-8"  src="https://uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" />
+                    <img className="w-8 h-8"  src="https://uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" />
+                    <img className="w-8 h-8"  src="https://uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" />
+                    <img className="w-8 h-8" src="https://uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-empty-icon.png" alt="" />
                 </div>
             )
         break;
         case 5:
             return(
-                <div className="flex">
-                    <img className="w-10 h-10"  src="https://uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" />
-                    <img className="w-10 h-10"  src="https://uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" />
-                    <img className="w-10 h-10"  src="https://uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" />
-                    <img className="w-10 h-10"  src="https://uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" />
-                    <img className="w-10 h-10"  src="https://uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" />
+                <div className="flex gap-1">
+                    <img className="w-8 h-8"  src="https://uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" />
+                    <img className="w-8 h-8"  src="https://uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" />
+                    <img className="w-8 h-8"  src="https://uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" />
+                    <img className="w-8 h-8"  src="https://uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" />
+                    <img className="w-8 h-8"  src="https://uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" />
                 </div>
             )
         break;
--- componentes/skills.tsx
+++ componentes/skills.tsx
@@ -16,15 +16,15 @@ export const Skills = () => {
         <div>
             {SessCtx?.sessoes[0].hab && 
                 <HeaderSess title="Habilidades" Func={SessCtx?.handlehab}>
-                    <div className="grid grid-cols-1 gap-2 px-2">
+                    <div className="grid grid-cols-1 gap-8 md:grid-cols-2 md:px-25 xl:px-40 lg:grid-cols-3 xl:grid-cols-4 xl:pt-10 2xl:px-100 2xl:grid-cols-6 pb-10">
                         {Ctx?.hab.map((item) => {
                             return(
-                            <div key={item.id} className="border border-white p-4 flex flex-col items-center">
+                            <div key={item.id} className="border border-white p-4 flex flex-col items-center w-60 mx-auto">
                                 <img src={item.img} className="w-30 h-30">
 
                                 </img>
-                                <h2 className="font-Oswald text-3xl">{item.title}</h2>
-                                <div className="font-Oswald text-xl">Proficiência: {item.prof}.0/5.0</div>
+                                <h2 className="font-Oswald text-3xl mt-2">{item.title}</h2>
+                                <div className="font-Oswald text-xl my-2">Proficiência: {item.prof}.0/5.0</div>
                                 <Nota nota={item.prof}></Nota>
                             </div>
                             
