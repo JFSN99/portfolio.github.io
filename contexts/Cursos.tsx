c9b3146ef40cbeeb6bcca5d4da4ef3d21c9f3ed1
Juan <juanfsn1999@gmail.com>
2026-09-25 15:52:30 -0300

    'Ajustando+imgs'

--- contexts/Cursos.tsx
+++ contexts/Cursos.tsx
@@ -17,76 +17,76 @@ export const CrsCtx = ({children}:{children:ReactNode}) => {
     const Certificados = [
         {
         id:1,    
-        src:'/b7_burger.png',
+        src:'/portfolio.github.io/b7_burger.png',
         nome:'B7 burger',
         duracao:3   
         },
         {
         id:2,
-        src:'/fund_html_css.png',
+        src:'/portfolio.github.io/fund_html_css.png',
         nome:'Fundamentos de HTML e CSS',
         duracao:12
         },
         {
         id:3,
-        src:'/Fundamentos_git.png',
+        src:'/portfolio.github.io/Fundamentos_git.png',
         nome:'Fundamentos de Git',
         duracao:3
         },
         {
         id:4,    
-        src:'/fundamentos_java.png',
+        src:'/portfolio.github.io/fundamentos_java.png',
         nome:'Fundamentos de Javascript',
         duracao:12,
         },
         {
         id:5,
-        src:'/Fundamentos_reactJs.png',
+        src:'/portfolio.github.io/Fundamentos_reactJs.png',
         nome:'Fundamentos de React.Js',
         duracao:12
         },
         {
         id:6,    
-        src:'/Fundamentos_tailwind.png',
+        src:'/portfolio.github.io/Fundamentos_tailwind.png',
         nome:'Fundamentos de Tailwind',
         duracao:3
         },
         {
         id:7,
-        src:'/GitHub_na_pratica.png',
+        src:'/portfolio.github.io/GitHub_na_pratica.png',
         nome:'GitHub na Prática',
         duracao:5
         },
         {
         id:8,
-        src:'/html_css_avan.png',
+        src:'/portfolio.github.io/html_css_avan.png',
         nome:'HTML/CSS Avançados',
         duracao:24},
         {
         id:9,
-        src:'/java_avanc.png',
+        src:'/portfolio.github.io/java_avanc.png',
         nome:'Javascript Avançado',
         duracao:17
         }, 
         {
         id:10,
-        src:'/projeto_starbucks.png',
+        src:'/portfolio.github.io/projeto_starbucks.png',
         nome:'Projeto Starbucks',
         duracao:3},
         {
         id:11,    
-        src:'/react_int.png',
+        src:'/portfolio.github.io/react_int.png',
         nome:'React Intermediário',
         duracao:12
         },
         {
         id:12,    
-        src:'/Responsividade_tailwind.png',
+        src:'/portfolio.github.io/Responsividade_tailwind.png',
         nome:'Responsividade com Tailwind',
         duracao:17},
         {
         id:13,
-        src:'/Typescript_do_zero.png',
+        src:'/portfolio.github.io/Typescript_do_zero.png',
         nome:'TypeScript do zero',
         duracao:5},
     ]
